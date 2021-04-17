603b807bf9311ebbd5e6fb92a17c36758092ff19
Munguu <mungu0623@gmail.com>
2021-04-17 09:54:28 -0700

    multimedia web site hiij duusgasan

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  cursor: {
+    pointer: "pointer",
+  },
+  variants: {
+    display: ["responsive", "group-hover"],
+  },
+  plugins: [],
+};
